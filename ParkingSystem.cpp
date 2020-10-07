eb21965f9ded48b0dbfbc6b270b5ed0e8455780a
zL1nX <linxzhan9@gmail.com>
2020-10-07 21:18:08 +0800

    1603-设计停车系统

---
+++ ParkingSystem.cpp
@@ -0,0 +1,25 @@
+class ParkingSystem {
+private:
+    int space[3] = { 0 };
+    
+public:
+    ParkingSystem(int big, int medium, int small) {
+        space[0] = big;
+        space[1] = medium;
+        space[2] = small;
+    }
+    
+    bool addCar(int carType) {
+        if(space[carType - 1] > 0){
+            space[carType - 1] --;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
