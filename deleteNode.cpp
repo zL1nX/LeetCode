75733e6146240bd19427cf9b8467fa6ab01e01d1
zL1nX <linxzhan9@gmail.com>
2020-10-08 19:24:29 +0800

    面试0203-删除中间节点

---
+++ deleteNode.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+
+/*
+实现一种算法，删除单向链表中间的某个节点（即不是第一个或最后一个节点），假定你只能访问该节点。
+
+ 
+
+示例：
+
+输入：单向链表a->b->c->d->e->f中的节点c
+结果：不返回任何数据，但该链表变为a->b->d->e->f
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/delete-middle-node-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *p = node->next;
+        node->val = p->val;
+        node->next = p->next;
+        // 将下一个节点的值复制到当前节点，然后删除下一个节点
+        // 等价于删除了当前节点
+    }
+};
