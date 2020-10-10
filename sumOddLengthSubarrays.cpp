3901eae83a8daf25f9cdb81ce3be052ba48f7115
zL1nX <linxzhan9@gmail.com>
2020-10-10 19:08:11 +0800

    1588-所有奇数长度子数组的和

---
+++ sumOddLengthSubarrays.cpp
@@ -0,0 +1,60 @@
+/*
+
+给你一个正整数数组 arr ，请你计算所有可能的奇数长度子数组的和。
+
+子数组 定义为原数组中的一个连续子序列。
+
+请你返回 arr 中 所有奇数长度子数组的和 。
+
+ 
+
+示例 1：
+
+输入：arr = [1,4,2,5,3]
+输出：58
+解释：所有奇数长度子数组和它们的和为：
+[1] = 1
+[4] = 4
+[2] = 2
+[5] = 5
+[3] = 3
+[1,4,2] = 7
+[4,2,5] = 11
+[2,5,3] = 10
+[1,4,2,5,3] = 15
+我们将所有值求和得到 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+示例 2：
+
+输入：arr = [1,2]
+输出：3
+解释：总共只有 2 个长度为奇数的子数组，[1] 和 [2]。它们的和为 3 。
+示例 3：
+
+输入：arr = [10,11,12]
+输出：66
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sum-of-all-odd-length-subarrays
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <vector>
+using namespace std;
+
+int sumOddLengthSubarrays(vector<int>& arr) 
+{
+    int sum = 0;
+    for(int w = 1; w <= arr.size(); w += 2)
+    {
+        for(int o = 0; o < arr.size() + 1 - w; o++)
+        {
+            int width = w;
+            for(auto start = arr.begin() + o; width > 0; start++ )
+            {
+                sum += *start;
+                width--;
+            }
+        }
+    }
+    return sum;
+}
