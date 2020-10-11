f3cb5692c74df6c535136090bb7b19457eba7b3f
zL1nX <linxzhan9@gmail.com>
2020-10-11 19:34:15 +0800

    905-按奇偶排序数组

---
+++ sortArrayByParity.cpp
@@ -0,0 +1,79 @@
+/*
+给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。
+
+你可以返回满足此条件的任何数组作为答案。
+
+ 
+
+示例：
+
+输入：[3,1,2,4]
+输出：[2,4,3,1]
+输出 [4,2,3,1]，[2,4,1,3] 和 [4,2,1,3] 也会被接受。
+ 
+
+提示：
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+
+*/
+#include <vector>
+using namespace std;
+vector<int> sortArrayByParity(vector<int>& A) 
+{
+    // vector<int> res;
+    // for(int i = 0; i < A.size(); i ++)
+    // {
+    //     if(A[i] % 2)
+    //     {
+    //         res.push_back(A[i]);
+    //     }
+    //     else
+    //     {
+    //         res.insert(res.begin(), A[i]);
+    //     }
+    // }
+    // return res;
+    int p = 0, q = A.size() - 1;
+    while(p < q)
+    {
+        if(A[p] % 2 && A[q] % 2 == 0)
+        {
+            swap(A[p], A[q]);
+        }
+        else if(A[p] % 2 && A[q] % 2)
+        {
+            q--;
+        }
+        else if(A[p] % 2 == 0 && A[q] % 2 == 0)
+        {
+            p++;
+        }
+    }
+    return A;
+}
+
+/*
+
+int p = 0, q = A.size() - 1;
+    while(p != q)
+    {
+        if(A[p] % 2 && A[q] % 2 == 0)
+        {
+            swap(A[p], A[q]);
+        }
+        else if(A[p] % 2 && A[q] % 2)
+        {
+            swap(A[p], A[--q]);
+        }
+        else if(A[p] % 2 == 0 && A[q] % 2 == 0)
+        {
+            swap(A[++p], A[q]);
+        }
+        p++;
+        q--;
+    }
+    return A;
+
+*/
