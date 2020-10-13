a4a7dd3fae9abed93ee003ad5dbc34223a1568f1
zL1nX <linxzhan9@gmail.com>
2020-10-13 16:30:59 +0800

    1290-二进制链表转整数

---
+++ getDecimalValue.cpp
@@ -0,0 +1,49 @@
+/*
+
+给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+
+请你返回该链表所表示数字的 十进制值 。
+
+输入：head = [1,0,1]
+输出：5
+解释：二进制数 (101) 转化为十进制数 (5)
+示例 2：
+
+输入：head = [0]
+输出：0
+示例 3：
+
+输入：head = [1]
+输出：1
+示例 4：
+
+输入：head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+输出：18880
+示例 5：
+
+输入：head = [0,0]
+输出：0
+
+*/
+#include <vector>
+#include <math.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int getDecimalValue(ListNode* head) 
+{
+    ListNode *p = head;
+    int nums = 0;
+    while(p)
+    {
+        nums = 2 * nums + p->val; // 正序的二进制转10进制
+        p = p->next;
+    }
+    return nums;
+}
