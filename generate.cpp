516de09225a3c1bf6b219187dcd9641ccb483090
zL1nX <linxzhan9@gmail.com>
2020-12-06 10:19:27 +0800

    118-杨辉三角

---
+++ generate.cpp
@@ -0,0 +1,38 @@
+/*
+给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 5
+输出:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/pascals-triangle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+#include <vector>
+using namespace std;
+
+vector<vector<int>> generate(int numRows) 
+{
+    vector<vector<int>> triangle;
+    for(int i = 1; i <= numRows; i ++)
+    {
+        vector<int> row(i, 1);
+        for(int j = 1; j < i - 1; j ++)
+        {
+            row[j] = triangle[i - 1 - 1][j - 1] + triangle[i - 1 - 1][j];
+        }
+        triangle.push_back(row);
+    }
+    return triangle;
+}
