301ee67e4f9d6ba1c8239a6200c55f40e3b3cda5
zL1nX <linxzhan9@gmail.com>
2020-12-17 23:25:58 +0800

    面试题01.02-判定是否互为字符重排

---
+++ CheckPermutation.cpp
@@ -0,0 +1,31 @@
+/*
+
+给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+
+示例 1：
+
+输入: s1 = "abc", s2 = "bca"
+输出: true 
+示例 2：
+
+输入: s1 = "abc", s2 = "bad"
+输出: false
+说明：
+
+0 <= len(s1) <= 100
+0 <= len(s2) <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/check-permutation-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+#include <string>
+using namespace std;
+
+bool CheckPermutation(string s1, string s2) 
+{
+    sort(s1.begin(), s1.begin());
+    sort(s2.begin(), s2.begin());
+    return s1 == s2;
+}
