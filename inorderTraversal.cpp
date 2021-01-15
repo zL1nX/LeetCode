0439bdc2b2a1b70dcc176cf6e798d6d42d155904
zL1nX <linxzhan9@gmail.com>
2021-01-15 20:53:16 +0800

    94-二叉树的中序遍历

---
+++ inorderTraversal.cpp
@@ -0,0 +1,74 @@
+/*
+给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,3,2]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+示例 4：
+
+
+输入：root = [1,2]
+输出：[2,1]
+示例 5：
+
+
+输入：root = [1,null,2]
+输出：[1,2]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <vector>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<int> res;
+
+void traverse(TreeNode* node)
+{
+    if(node == nullptr)
+    {
+        return;
+    }
+    traverse(node->left);
+    res.push_back(node->val);
+    traverse(node->right);
+
+}
+
+vector<int> inorderTraversal(TreeNode* root) 
+{
+    traverse(root);
+    return res; 
+}
+
+// 用迭代的方法模拟递归（维护一个栈，观察节点顺序）
