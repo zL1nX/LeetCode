d7bfc500b9735c9681bcdc615174db7d490bb4c7
zL1nX <linxzhan9@gmail.com>
2021-02-22 13:34:16 +0800

    700-二叉搜索树中的搜索

---
+++ searchBST.cpp
@@ -0,0 +1,56 @@
+/*
+给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+
+例如，
+
+给定二叉搜索树:
+
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+和值: 2
+你应该返回如下子树:
+
+      2     
+     / \   
+    1   3
+在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+
+通过次数49,144提交次数65,228
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-in-a-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* searchBST(TreeNode* root, int val) 
+{
+    if(root == nullptr)
+    {
+        return nullptr;
+    }
+    if(root->val == val)
+    {
+        return root;
+    }
+    else if(root->val > val) // 注意寻找的方向
+    {
+        return searchBST(root->left, val);
+    }
+    else
+    {
+        return searchBST(root->right, val);
+    }
+}
