5ab3477fd30ae2c7b8097c782314ca629bb0d2fd
zL1nX <linxzhan9@gmail.com>
2021-03-26 09:42:17 +0800

    83. 删除排序链表中的重复元素

---
+++ deleteDuplicates2.cpp
@@ -0,0 +1,59 @@
+/*
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+
+返回同样按升序排列的结果链表。
+
+ 
+
+示例 1：
+
+
+输入：head = [1,1,2]
+输出：[1,2]
+示例 2：
+
+
+输入：head = [1,1,2,3,3]
+输出：[1,2,3]
+ 
+
+提示：
+
+链表中节点数目在范围 [0, 300] 内
+-100 <= Node.val <= 100
+题目数据保证链表已经按升序排列
+通过次数206,217提交次数392,048
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* deleteDuplicates(ListNode* head) 
+{
+    if(!head)
+    {
+        return head; // 返回head而不是null，因为一个节点的话，不能返回null
+    }
+    ListNode* curr = head;
+    while(curr->next)
+    {
+        if(curr->val == curr->next->val)
+        {
+            curr->next = curr->next->next;
+        }
+        else
+        {
+            curr = curr->next;
+        }   
+    }
+    return head;
+}
