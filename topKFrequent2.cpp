91b97079cd8d8668272f05bbc097ce827731a914
zL1nX <linxzhan9@gmail.com>
2021-05-20 10:52:27 +0800

    692-前K个高频单词2

---
+++ topKFrequent2.cpp
@@ -0,0 +1,124 @@
+/*
+给一非空的单词列表，返回前 k 个出现次数最多的单词。
+
+返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
+
+示例 1：
+
+输入: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+输出: ["i", "love"]
+解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+    注意，按字母顺序 "i" 在 "love" 之前。
+ 
+
+示例 2：
+
+输入: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+输出: ["the", "is", "sunny", "day"]
+解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
+    出现次数依次为 4, 3, 2 和 1 次。
+ 
+
+注意：
+
+假定 k 总为有效值， 1 ≤ k ≤ 集合元素数。
+输入的单词均由小写字母组成。
+ 
+
+扩展练习：
+
+尝试以 O(n log k) 时间复杂度和 O(n) 空间复杂度解决。
+通过次数34,012提交次数62,961
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/top-k-frequent-words
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <vector>
+#include <queue>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+struct cmp // 注意要使用这种排序方式，但排好序后在优先队列中是反着来的，即因为是队列，所以队首是那个最弱的元素
+{
+    bool operator()(pair<string, int> a, pair<string, int> b)
+    {
+        if(a.second != b.second)
+        {
+            return a.second > b.second;
+        }
+        else
+        {
+            return a.first < b.first;
+        }
+    }
+};
+
+vector<string> topKFrequent(vector<string>& words, int k)
+{
+    unordered_map<string, int> word_map;
+    priority_queue<pair<string, int>, vector<pair<string, int>>, cmp> topk; // 注意这里优先队列的声明
+    for(string word : words)
+    {
+        word_map[word] ++;
+    }
+
+    for(const pair<string, int> it : word_map)
+    {
+        topk.push(it);
+        if(topk.size() > k)
+        {
+            topk.pop();
+        }
+    }
+    vector<string> res(k);
+    for(int i = k - 1; i >= 0; i --) // 最后需要倒序排一下
+    {
+        res[i] = topk.top().first;
+        topk.pop();
+    }
+    return res;
+}
+
+// struct cmp // 
+// {
+//     bool operator()(pair<string, int> a, pair<string, int> b)
+//     {
+//         if(a.second != b.second)
+//         {
+//             return a.second < b.second;
+//         }
+//         else
+//         {
+//             return a.first > b.first;
+//         }
+//     }
+// };
+
+// vector<string> topKFrequent(vector<string>& words, int k)
+// {
+//     unordered_map<string, int> word_map;
+//     priority_queue<pair<string, int>, vector<pair<string, int>>, cmp> topk; // 
+//     for(string word : words)
+//     {
+//         word_map[word] ++;
+//     }
+
+//     for(const pair<string, int> it : word_map)
+//     {
+//         topk.push(it);
+//         if(topk.size() > k)
+//         {
+//             topk.pop();
+//         }
+//     }
+//     vector<string> res;
+//     while(k--)
+//     {
+//         res.push_back(topk.top().first);
+//         topk.pop();
+//     }
+//     return res;
+// }
