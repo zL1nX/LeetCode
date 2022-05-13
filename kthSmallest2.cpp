4e0eefcdba37313cb0b00fc0f339d15960ff773e
XiaoLin <xiaolinzhang@stu.xidian.edu.cn>
2022-05-13 19:56:50 +0800

    230-二叉搜索树中第K小的元素

---
+++ kthSmallest2.cpp
@@ -0,0 +1,68 @@
+/*
+给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,1,4,null,2], k = 1
+输出：1
+示例 2：
+
+
+输入：root = [5,3,6,2,4,null,null,1], k = 3
+输出：3
+ 
+
+ 
+
+提示：
+
+树中的节点数为 n 。
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+ 
+
+进阶：如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化算法？
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/kth-smallest-element-in-a-bst
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int rk = 0, res = 0;
+
+int kthSmallest(TreeNode* root, int k) 
+{
+    traverse(root, k);
+    return res;
+}
+
+void traverse(TreeNode* node, int k)
+{
+    if(node == nullptr)
+    {
+        return;
+    }
+    traverse(node->left, k);
+    rk++;
+    if(rk == k)
+    {
+        res = node->val;
+    }
+    traverse(node->right, k);
+}
